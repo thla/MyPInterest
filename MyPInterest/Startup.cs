32c192604128ff01be059a84e6952e0a4a977f21
Thomas Lamparter <thomas.lamparter@silverstroke.com>
2017-10-03 23:16:43 +0200

    Twitter Authentication added

--- MyPInterest/Startup.cs
+++ MyPInterest/Startup.cs
@@ -33,6 +33,12 @@ namespace MyPInterest
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+            services.AddAuthentication().AddTwitter(twitterOptions =>
+            {
+                twitterOptions.ConsumerKey = Configuration["Authentication:Twitter:ConsumerKey"];
+                twitterOptions.ConsumerSecret = Configuration["Authentication:Twitter:ConsumerSecret"];
+            });
+
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
 
